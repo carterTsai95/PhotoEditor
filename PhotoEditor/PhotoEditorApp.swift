4a091dfa49101fb597f40d878aba46a8cf364f1a
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2021-05-17 18:17:39 -0400

    Initial Commit

---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by Hung-Chun Tsai on 2021-05-17.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
